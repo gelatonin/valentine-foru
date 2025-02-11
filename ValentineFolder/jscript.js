3e2a4f6d8a0d93148c7a76520f080cb2dc38902c
gelatonin <leabresizach@gmail.com>
2025-02-11 10:16:13 +0800

    Delete ValentineFolder directory

--- ValentineFolder/jscript.js
+++
@@ -1,4 +0,0 @@
-function goToPage(pageId) {
-            document.querySelectorAll('.container').forEach(page => page.classList.remove('active'));
-            document.getElementById(pageId).classList.add('active');
-}
